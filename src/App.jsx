ed495aa0eea17cad15e63d39f26e21cb1084b9b6
sheweliz <antinerosheweliz02@gmail.com>
2025-10-14 11:39:23 +0800

    course

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
+import Course from "./pages/Course";
 // import About from "./pages/About";
-// import Course from "./pages/Course";
 // import Admission from "./pages/Admission";
 // import Gallery from "./pages/Gallery";
 // import Contact from "./pages/Contact";
@@ -19,8 +19,8 @@ const App = () => {
         <div className="flex-grow-1">
           <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/about" element={<About />} />
             <Route path="/course" element={<Course />} />
+            {/* <Route path="/about" element={<About />} />
             <Route path="/admission" element={<Admission />} />
             <Route path="/gallery" element={<Gallery />} />
             <Route path="/contact" element={<Contact />} /> */}
